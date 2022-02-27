807aa75c380582073064080d3772fc5f33785f74
juanmi-gh <juancabrera.dev@gmail.com>
2022-02-27 20:26:10 +0100

    Create image

--- jm.cabrerar.2021/src/main/java/es/codeurjc/mca/practica_1_cloud_ordinaria_2021/image/S3ImageService.java
+++ jm.cabrerar.2021/src/main/java/es/codeurjc/mca/practica_1_cloud_ordinaria_2021/image/S3ImageService.java
@@ -1,21 +1,35 @@
 package es.codeurjc.mca.practica_1_cloud_ordinaria_2021.image;
 
+import static java.lang.System.getProperty;
+import static software.amazon.awssdk.core.sync.RequestBody.fromFile;
+
+import java.io.File;
+import java.util.UUID;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Profile;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.server.ResponseStatusException;
+import software.amazon.awssdk.core.sync.RequestBody;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
 import software.amazon.awssdk.services.s3.model.CreateBucketRequest;
 import software.amazon.awssdk.services.s3.model.HeadBucketRequest;
 import software.amazon.awssdk.services.s3.model.NoSuchBucketException;
+import software.amazon.awssdk.services.s3.model.PutObjectRequest;
 
 @Slf4j
 @Service("storageService")
 @Profile("production")
 public class S3ImageService implements ImageService {
+
+    private static final String S3_PUBLIC = "https://s3.amazonaws.com/";
+
+    @Value("${amazon.s3.bucket-name}")
+    private String bucketName;
 
     private S3Client s3;
 
@@ -33,8 +47,29 @@ public class S3ImageService implements ImageService {
 
     @Override
     public String createImage(MultipartFile multiPartFile) {
-        // Not implemented yet
-        return null;
+
+        try {
+            log.info("Uploading image to S3");
+
+            var file = new File(getProperty("java.io.tmpdir") + "/" + multiPartFile.getOriginalFilename());
+            multiPartFile.transferTo(file);
+
+            final var fileName = "image_" + UUID.randomUUID() + "_" + multiPartFile.getOriginalFilename();
+
+            var putRequest = PutObjectRequest.builder()
+                .bucket(bucketName)
+                .key(fileName)
+                .build();
+
+            var response = s3.putObject(putRequest, fromFile(file));
+
+            log.info("SDK HTTP response: {}", response.sdkHttpResponse().statusCode());
+
+            return S3_PUBLIC + bucketName + "/" + fileName;
+
+        } catch (Exception ex) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Can't upload image.", ex);
+        }
     }
 
     @Override
