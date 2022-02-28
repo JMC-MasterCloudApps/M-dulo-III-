a992d7b23828e23b7987e640155ecfc34fec59c2
juanmi-gh <juancabrera.dev@gmail.com>
2022-02-28 10:53:44 +0100

    Create RDS database

--- jm.cabrerar.2021/runProduction.sh
+++ jm.cabrerar.2021/runProduction.sh
@@ -1,7 +1,7 @@
 mvn package -DskipTests
 
 export BUCKET_NAME=practica-1.cloud.jm.cabrerar.2021
-export RDS_ENDPOINT=
+export RDS_ENDPOINT=jm-cabrerar-2021-events-db.cp1qc6z599xl.us-east-1.rds.amazonaws.com:3306
 
 export REGION=us-east-1
 export RDS_DATABASE=events_db
